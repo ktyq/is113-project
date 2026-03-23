d3927e883f15bee742677b20646222d661a716a3
agnellla <153419686+agnellla@users.noreply.github.com>
2026-03-24 02:23:59 +0800

    edited movie.ejs and routers

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const session = require('express-session');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const session = require("express-session");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const mongoose = require("mongoose");
+const path = require("path");
 // const fs = require('fs');
 
 const server = express();
@@ -18,30 +18,31 @@ server.use(express.json());
 //     resave: false,  // Prevents the session from being saved back to the session store if nothing has changed.
 //     saveUninitialized: false  // Prevents a new, empty session from being saved to the store.
 // }));
-server.get('/', (req, res) => res.status(200).send(`success!`));
+server.get("/", (req, res) => res.status(200).send(`success!`));
 
 // routes
-server.use('/list', require('./routes/watchlist'));
+server.use("/list", require("./routes/watchlist"));
+server.use("/reviews", require("./routes/reviews"));
 
 async function connectDB() {
-    try {
-        // connecting to Database with our config.env file and DB is constant in config.env
-        await mongoose.connect(process.env.DB);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error.message);
-        process.exit(1);
-    }
-};
+  try {
+    // connecting to Database with our config.env file and DB is constant in config.env
+    await mongoose.connect(process.env.DB);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+}
 
 function startServer() {
-    const hostname = "localhost"; // Define server hostname
-    const port = 8000;// Define port number
+  const hostname = "localhost"; // Define server hostname
+  const port = 8000; // Define port number
 
-    // Start the server and listen on the specified hostname and port
-    server.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+  // Start the server and listen on the specified hostname and port
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 }
 
 connectDB().then(startServer);
