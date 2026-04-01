ecebf02bdbd90b31b08b061a3c51d88e41c43f8b
Peijin-Tan <peijin.tan.2025@computing.smu.edu.sg>
2026-04-01 15:52:17 +0800

    addindexcontroller

---
+++ controllers/indexController.js
@@ -0,0 +1,37 @@
+const Movie = require('../models/Movie');
+
+// --- DISPLAY ALL MOVIES ---
+exports.getAllMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find().sort({ createdAt: -1 });
+
+        res.render('index', { 
+            movies, 
+            user: req.session.user || null 
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error fetching movies");
+    }
+};
+
+// --- DISPLAY SINGLE MOVIE ---
+exports.getMovieById = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+
+        if (!movie) {
+            return res.status(404).send("Movie not found");
+        }
+
+        res.render('movie', { 
+            movie, 
+            user: req.session.user || null 
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+};
