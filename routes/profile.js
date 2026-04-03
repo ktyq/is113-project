990f7776bf625cb8cbf1f522cddcbd97683639d9
KT <116776991+ktyq@users.noreply.github.com>
2026-04-03 16:37:22 +0800

    routes comments

--- routes/profile.js
+++ routes/profile.js
@@ -1,39 +1,41 @@
-// routes/profile.js - hazel
+// routes/profile.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authentication');
 
-//--Root redirect--//
+// Redirect from root
 router.get('/', (req, res) => {
     if (req.session.user) {
         return req.session.user.role === 'admin' || req.session.user.role === 'superadmin'
-        ? res.redirect('/profile')
-        : res.redirect('/index');
+            ? res.redirect('/profile')
+            : res.redirect('/index');
     }
     res.redirect('/index');
-})
+});
 
+// Registration
 router.get('/register', userController.registerGet);
 router.post('/register', userController.registerPost);
 
+// User login
 router.get('/login', userController.loginGet);
 router.post('/login', userController.loginPost);
 
-//--User profile--//
+// View user profile
 router.get('/profile', authMiddleware.isLoggedIn, userController.profile);
 
-//--Edit profile--//
+// Editing user profile
 router.get('/edit-profile', authMiddleware.isLoggedIn, userController.editProfileGet);
 router.post('/edit-profile', authMiddleware.isLoggedIn, userController.editProfilePost);
 
-//--Delete profile--//
+// Delete user profile
 router.post('/delete-profile', authMiddleware.isLoggedIn, userController.deleteProfile);
 
-//--Logout--//
+// Log out
 router.post('/logout', userController.logout);
 
-//--Superadmin--//
+// Superadmin
 router.get('/manage-accounts', authMiddleware.isAdmin, userController.manageAccountsGet);
 router.post('/manage-accounts/promote', authMiddleware.isAdmin, userController.promoteToAdmin);
 router.post('/manage-accounts/demote', authMiddleware.isAdmin, userController.demoteToUser);
