a97b81f9c8fec9ab2216801e548c3324465c6138
hazellng <hzlnguyen03@gmail.com>
2026-04-03 17:58:19 +0800

    edit

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -14,7 +14,7 @@ exports.isLoggedIn = async (req, res, next) => {
 
         // Refresh session user data with latest from database
         req.session.user = {
-            id: user._id,
+            id: user._id.toString(),
             username: user.username,
             email: user.email,
             role: user.role,
